8d69357abe18dddef107e07cd9660d66fe47ab89
Yokinman <yokinmun@gmail.com>
2023-08-20 01:35:18 -0400

    Renamed {Fraction, FractionTerm} to {Frac, FracTerm} for convenience. Trimming latter syllables is neat when it doesn't add ambiguity.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::fmt::{Debug, Display, Formatter};
 use std::ops::{Add, Sub, Mul, Div, Neg};
 
 /// ...
-pub trait FractionTerm: PartialOrd + Copy + private::Sealed {
+pub trait FracTerm: PartialOrd + Copy + private::Sealed {
 	const MAX_TERM_COUNT: usize;
 	
 	fn from_f64(num: f64) -> Self;
@@ -32,7 +32,7 @@ mod private {
 macro_rules! impl_fraction_term {
 	($type:ty) => {
 		impl private::Sealed for $type {}
-		impl FractionTerm for $type {
+		impl FracTerm for $type {
 			const MAX_TERM_COUNT: usize = (1 + ((Self::MAX.ilog2() + 1) / 2)) as usize;
 			
 			fn from_f64(num: f64) -> Self { num as Self }
@@ -55,12 +55,12 @@ impl_fraction_term!(u128);
 
 /// ...
 #[derive(Copy, Clone, Debug)]
-pub enum Fraction<T: FractionTerm> {
+pub enum Frac<T: FracTerm> {
 	Pos(T, T),
 	Neg(T, T),
 }
 
-impl<T: FractionTerm> Fraction<T> {
+impl<T: FracTerm> Frac<T> {
 	pub fn numer(&self) -> T {
 		match *self {
 			Self::Pos(n, _) => n,
@@ -145,28 +145,28 @@ impl<T: FractionTerm> Fraction<T> {
 	}
 }
 
-impl<T: FractionTerm> Default for Fraction<T> {
+impl<T: FracTerm> Default for Frac<T> {
 	fn default() -> Self {
 		Self::Pos(T::from_f64(1.0), T::from_f64(1.0))
 	}
 }
 
-impl<T: FractionTerm> From<Fraction<T>> for (T, T) {
-	fn from(value: Fraction<T>) -> Self {
+impl<T: FracTerm> From<Frac<T>> for (T, T) {
+	fn from(value: Frac<T>) -> Self {
 		match value {
-			Fraction::Pos(n, d) => (n, d),
-			Fraction::Neg(n, d) => (n, d),
+			Frac::Pos(n, d) => (n, d),
+			Frac::Neg(n, d) => (n, d),
 		}
 	}
 }
 
-impl<T: FractionTerm> PartialEq<Self> for Fraction<T> {
+impl<T: FracTerm> PartialEq<Self> for Frac<T> {
 	fn eq(&self, other: &Self) -> bool {
 		self.partial_cmp(other) == Some(Ordering::Equal)
 	}
 }
 
-impl<T: FractionTerm + PartialOrd> PartialOrd<Self> for Fraction<T> {
+impl<T: FracTerm + PartialOrd> PartialOrd<Self> for Frac<T> {
 	//! - 0/0 != 0/0
 	//! - A/N <> B/N === A <> B
 	//! - A/B <> C/D === AD <> CB
@@ -221,35 +221,35 @@ impl<T: FractionTerm + PartialOrd> PartialOrd<Self> for Fraction<T> {
 	}
 }
 
-impl<T: FractionTerm> Add for Fraction<T> {
+impl<T: FracTerm> Add for Frac<T> {
 	type Output = Self;
 	fn add(self, rhs: Self) -> Self::Output {
 		self.checked_add(rhs).unwrap()
 	}
 }
 
-impl<T: FractionTerm> Sub for Fraction<T> {
+impl<T: FracTerm> Sub for Frac<T> {
 	type Output = Self;
 	fn sub(self, rhs: Self) -> Self::Output {
 		self.checked_sub(rhs).unwrap()
 	}
 }
 
-impl<T: FractionTerm> Mul for Fraction<T> {
+impl<T: FracTerm> Mul for Frac<T> {
 	type Output = Self;
 	fn mul(self, rhs: Self) -> Self::Output {
 		self.checked_mul(rhs).unwrap()
 	}
 }
 
-impl<T: FractionTerm> Div for Fraction<T> {
+impl<T: FracTerm> Div for Frac<T> {
 	type Output = Self;
 	fn div(self, rhs: Self) -> Self::Output {
 		self.checked_div(rhs).unwrap()
 	}
 }
 
-impl<T: FractionTerm> Neg for Fraction<T> {
+impl<T: FracTerm> Neg for Frac<T> {
 	type Output = Self;
 	fn neg(self) -> Self::Output {
 		match self {
@@ -259,25 +259,25 @@ impl<T: FractionTerm> Neg for Fraction<T> {
 	}
 }
 
-impl<T: FractionTerm> From<f32> for Fraction<T> {
+impl<T: FracTerm> From<f32> for Frac<T> {
 	//! Generates the closest fraction within the integer type's bounds.
 	
 	fn from(value: f32) -> Self {
-		Fraction::from(value as f64)
+		Frac::from(value as f64)
 	}
 }
 
-impl<T: FractionTerm> From<f64> for Fraction<T> {
+impl<T: FracTerm> From<f64> for Frac<T> {
 	//! Generates the closest fraction within the integer type's bounds.
 	//! 
 	//! # Examples
 	//! 
 	//! ```
-	//! use fraction::Fraction;
-	//! assert_eq!("1/3",      format!("{}", Fraction::<u8> ::from(0.333_f64)));
-	//! assert_eq!("333/1000", format!("{}", Fraction::<u16>::from(0.333_f64)));
-	//! assert_eq!("-50/1",    format!("{}", Fraction::<u64>::from(-50_f32)));
-	//! assert_eq!("22/7",     format!("{}", Fraction::<u8> ::from(std::f64::consts::PI)));
+	//! use fraction::Frac;
+	//! assert_eq!("1/3",      format!("{}", Frac::<u8> ::from(0.333_f64)));
+	//! assert_eq!("333/1000", format!("{}", Frac::<u16>::from(0.333_f64)));
+	//! assert_eq!("-50/1",    format!("{}", Frac::<u64>::from(-50_f32)));
+	//! assert_eq!("22/7",     format!("{}", Frac::<u8> ::from(std::f64::consts::PI)));
 	//! ```
 	
 	fn from(mut num: f64) -> Self {
@@ -341,25 +341,25 @@ impl<T: FractionTerm> From<f64> for Fraction<T> {
 	}
 }
 
-impl<T: FractionTerm> From<Fraction<T>> for f32 {
-	fn from(value: Fraction<T>) -> Self {
+impl<T: FracTerm> From<Frac<T>> for f32 {
+	fn from(value: Frac<T>) -> Self {
 		value.to_f64() as f32
 	}
 }
 
-impl<T: FractionTerm> From<Fraction<T>> for f64 {
-	fn from(value: Fraction<T>) -> Self {
+impl<T: FracTerm> From<Frac<T>> for f64 {
+	fn from(value: Frac<T>) -> Self {
 		value.to_f64()
 	}
 }
 
 macro_rules! impl_from_fraction_for_fraction {
 	($F:ty, $T:ty) => {
-		impl From<Fraction<$F>> for Fraction<$T>{
-			fn from(value: Fraction<$F>) -> Self {
+		impl From<Frac<$F>> for Frac<$T>{
+			fn from(value: Frac<$F>) -> Self {
 				match value {
-					Fraction::<$F>::Pos(n, d) => Self::Pos(n.into(), d.into()),
-					Fraction::<$F>::Neg(n, d) => Self::Neg(n.into(), d.into()),
+					Frac::<$F>::Pos(n, d) => Self::Pos(n.into(), d.into()),
+					Frac::<$F>::Neg(n, d) => Self::Neg(n.into(), d.into()),
 				}
 			}
 		}
@@ -380,7 +380,7 @@ impl_from_fraction_for_fraction!(u16, u128);
 impl_from_fraction_for_fraction!(u32, u128);
 impl_from_fraction_for_fraction!(u64, u128);
 
-impl<T: FractionTerm + Display> Display for Fraction<T> {
+impl<T: FracTerm + Display> Display for Frac<T> {
 	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
 		match self {
 			Self::Pos(n, d) => write!(f,  "{}/{}", n, d),
@@ -395,21 +395,21 @@ mod tests {
 	
 	#[test]
 	fn conversion() {
-		fn assert<I: FractionTerm + Display>() {
+		fn assert<I: FracTerm + Display>() {
 			for n in 1..=100 {
 				for d in 1..=100 {
 					let n: f64 = n.into();
 					let d: f64 = d.into();
-					let f = Fraction::<I>::from(n/d);
+					let f = Frac::<I>::from(n/d);
 					// println!("{n}/{d} -> {f}, {}", n/d);
 					let v: f64 = f.into();
 					assert!(v > (n-1.0)/d);
 					assert!(v < (n+1.0)/d);
 				}
 			}
-			assert_eq!(Fraction::<I>::from(f64::INFINITY).to_f64(), I::from_f64(f64::INFINITY).into_f64());
-			assert_eq!(Fraction::<I>::from(f64::NEG_INFINITY).to_f64(), -I::from_f64(f64::INFINITY).into_f64());
-			assert!(Fraction::<I>::from(f64::NAN).to_f64().is_nan());
+			assert_eq!(Frac::<I>::from(f64::INFINITY).to_f64(), I::from_f64(f64::INFINITY).into_f64());
+			assert_eq!(Frac::<I>::from(f64::NEG_INFINITY).to_f64(), -I::from_f64(f64::INFINITY).into_f64());
+			assert!(Frac::<I>::from(f64::NAN).to_f64().is_nan());
 		}
 		assert::<u8>();
 		assert::<u64>();
@@ -418,30 +418,30 @@ mod tests {
 	
 	#[test]
 	fn arithmetic() {
-		assert_eq!("17/15",  format!("{}", Fraction::Pos(1_u8, 3) + Fraction::Pos(4, 5)));
-		assert_eq!("-7/15",  format!("{}", Fraction::Pos(1_u16, 3) - Fraction::Pos(4, 5)));
-		assert_eq!("8/6",    format!("{}", Fraction::Pos(4_u128, 6) + Fraction::Pos(2, 3)));
-		assert_eq!("112/6",  format!("{}", Fraction::Pos(8_u8, 3) * Fraction::Pos(14, 2)));
-		assert_eq!("-10/50", format!("{}", Fraction::Neg(1_u8, 10) / Fraction::Pos(5, 10)));
-		assert_eq!("2/0",    format!("{}", Fraction::Neg(4_u8, 0) + Fraction::Pos(6_u8, 0)));
+		assert_eq!("17/15",  format!("{}", Frac::Pos(1_u8, 3) + Frac::Pos(4, 5)));
+		assert_eq!("-7/15",  format!("{}", Frac::Pos(1_u16, 3) - Frac::Pos(4, 5)));
+		assert_eq!("8/6",    format!("{}", Frac::Pos(4_u128, 6) + Frac::Pos(2, 3)));
+		assert_eq!("112/6",  format!("{}", Frac::Pos(8_u8, 3) * Frac::Pos(14, 2)));
+		assert_eq!("-10/50", format!("{}", Frac::Neg(1_u8, 10) / Frac::Pos(5, 10)));
+		assert_eq!("2/0",    format!("{}", Frac::Neg(4_u8, 0) + Frac::Pos(6_u8, 0)));
 	}
 	
 	#[test]
 	fn comparison() {
-		assert_eq!(Fraction::Pos(5_u8, 5), Fraction::Pos(5_u8, 5));
-		assert_eq!(Fraction::Neg(1_u8, 10), Fraction::Neg(2_u8, 20));
-		assert!(Fraction::Pos(1_u8, 10) > Fraction::Neg(2_u8, 20));
-		assert!(Fraction::Neg(1_u8, 10) < Fraction::Pos(2_u8, 20));
-		assert!(Fraction::Neg(1_u8, 10) > Fraction::Neg(3_u8, 20));
-		assert!(Fraction::Neg(1_u8, 10) < Fraction::Neg(1_u8, 20));
-		assert_eq!(Fraction::Pos(u128::MAX - 1, u128::MAX), Fraction::Pos(u128::MAX - 1, u128::MAX));
-		assert!(Fraction::Pos(u128::MAX - 1, u128::MAX) < Fraction::Pos(u128::MAX - 1, u128::MAX - 1));
-		assert_eq!(Fraction::Pos(1_u8, 0), Fraction::Pos(1_u8, 0));
-		assert!(Fraction::Pos(2_u8, 0) > Fraction::Pos(1_u8, 0));
-		assert!(Fraction::Pos(1_u8, 0) > Fraction::Pos(1_u8, 1));
-		assert_eq!(Fraction::Pos(0_u8, 1), Fraction::Pos(0_u8, 2));
-		assert_eq!(Fraction::Pos(0_u8, 1), Fraction::Neg(0_u8, 1));
-		assert_ne!(Fraction::Pos(0_u8, 0), Fraction::Pos(0_u8, 0));
-		assert_ne!(Fraction::Pos(0_u8, 0), Fraction::Neg(0_u8, 0));
+		assert_eq!(Frac::Pos(5_u8, 5), Frac::Pos(5_u8, 5));
+		assert_eq!(Frac::Neg(1_u8, 10), Frac::Neg(2_u8, 20));
+		assert!(Frac::Pos(1_u8, 10) > Frac::Neg(2_u8, 20));
+		assert!(Frac::Neg(1_u8, 10) < Frac::Pos(2_u8, 20));
+		assert!(Frac::Neg(1_u8, 10) > Frac::Neg(3_u8, 20));
+		assert!(Frac::Neg(1_u8, 10) < Frac::Neg(1_u8, 20));
+		assert_eq!(Frac::Pos(u128::MAX - 1, u128::MAX), Frac::Pos(u128::MAX - 1, u128::MAX));
+		assert!(Frac::Pos(u128::MAX - 1, u128::MAX) < Frac::Pos(u128::MAX - 1, u128::MAX - 1));
+		assert_eq!(Frac::Pos(1_u8, 0), Frac::Pos(1_u8, 0));
+		assert!(Frac::Pos(2_u8, 0) > Frac::Pos(1_u8, 0));
+		assert!(Frac::Pos(1_u8, 0) > Frac::Pos(1_u8, 1));
+		assert_eq!(Frac::Pos(0_u8, 1), Frac::Pos(0_u8, 2));
+		assert_eq!(Frac::Pos(0_u8, 1), Frac::Neg(0_u8, 1));
+		assert_ne!(Frac::Pos(0_u8, 0), Frac::Pos(0_u8, 0));
+		assert_ne!(Frac::Pos(0_u8, 0), Frac::Neg(0_u8, 0));
 	}
 }
